0318f1e95d995051bf64af7bfe21fc139bc649a3
newcrossing <newcrossing@gmail.com>
2024-05-16 18:49:57 +0300

    Ссылки в ТГ, whatsapp, звонок

--- js/custom-scripts.js
+++ js/custom-scripts.js
@@ -198,23 +198,6 @@ jQuery(function($) {
         social_tools: false
     });
 
-    //Google Map
-    var latitude = $('#google-map').data('latitude');
-    var longitude = $('#google-map').data('longitude');
 
-    function initialize_map() {
-        var myLatlng = new google.maps.LatLng(latitude, longitude);
-        var mapOptions = {
-            zoom: 12,
-            scrollwheel: false,
-            center: myLatlng
-        };
-        var map = new google.maps.Map(document.getElementById('google-map'), mapOptions);
-        var marker = new google.maps.Marker({
-            position: myLatlng,
-            map: map
-        });
-    }
-    google.maps.event.addDomListener(window, 'load', initialize_map);
 
 });
